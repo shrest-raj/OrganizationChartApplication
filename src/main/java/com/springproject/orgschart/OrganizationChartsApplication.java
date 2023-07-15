b709023c72ab89984e70db3eeec7590492dab38c
Shrest Raj <shrestrajofficial@gmail.com>
2023-07-15 21:08:59 +0530

    enable opeapi documentation for the api's of the application

--- src/main/java/com/springproject/orgschart/OrganizationChartsApplication.java
+++ src/main/java/com/springproject/orgschart/OrganizationChartsApplication.java
@@ -8,17 +8,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
-* The class OrgschartApplication serves as a Spring Boot Application that facilitates automatic configuration
-* and initiates component scanning. It also acts as the entry point for the Employee Management System application.
-*
-* @author Shrest Raj
-* @version 1.0
-*/
+ * The class OrgschartApplication serves as a Spring Boot Application that facilitates automatic configuration
+ * and initiates component scanning. It also acts as the entry point for the Employee Management System application.
+ *
+ * @author Shrest Raj
+ * @version 1.0
+ */
 
 @SpringBootApplication
 @OpenAPIDefinition(
         info = @Info(
-                title="Organization Chart Application",
+                title = "Organization Chart Application",
                 version = "1.0.0",
                 description = "The Organization Charts project is a user-friendly Spring Boot application that streamlines employee tracking and management in organizations. \n ",
                 termsOfService = "runcodenow",
@@ -27,17 +27,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
                         email = "shrestrajofficial@gmail.com"
                 ),
                 license = @License(
-                        name="License",
-                        url="runcodenow"
+                        name = "License",
+                        url = "runcodenow"
+
                 )
         )
 )
 public class OrganizationChartsApplication {
     /**
-    * The main method serves as the entry point for the OrgsChartApplication
-    *
-    *@param args The command line arguments
-    */
+     * The main method serves as the entry point for the OrgsChartApplication
+     *
+     * @param args The command line arguments
+     */
 
     public static void main(String[] args) {
         SpringApplication.run(OrganizationChartsApplication.class, args);
