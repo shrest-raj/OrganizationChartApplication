6b655236efe43e533be562deb45efe8767253fd8
Shrest Raj <shrestrajofficial@gmail.com>
2023-07-17 13:13:21 +0530

    added error response class

---
+++ src/main/java/com/springproject/orgschart/exceptions/ErrorResponse.java
@@ -0,0 +1,32 @@
+package com.springproject.orgschart.exceptions;
+
+public class ErrorResponse {
+
+    private int errorCode;
+    private String errorMessage;
+    private String path;
+
+    public int getErrorCode() {
+        return errorCode;
+    }
+
+    public void setErrorCode(int errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+}
