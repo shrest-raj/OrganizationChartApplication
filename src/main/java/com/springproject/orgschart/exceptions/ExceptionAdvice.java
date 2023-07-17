2eb2100f756cfa06922ebcf762a50dc4141f36ad
Shrest Raj <shrestrajofficial@gmail.com>
2023-07-17 13:17:10 +0530

    added error response class

--- src/main/java/com/springproject/orgschart/exceptions/ExceptionAdvice.java
+++ src/main/java/com/springproject/orgschart/exceptions/ExceptionAdvice.java
@@ -3,14 +3,13 @@ package com.springproject.orgschart.exceptions;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.NoSuchElementException;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class ExceptionAdvice extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(DataNotFoundException.class)
